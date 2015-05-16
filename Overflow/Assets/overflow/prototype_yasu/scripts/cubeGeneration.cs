0d2077b458447f9a7aa2a9e4e6f96c5e72982cfb
Pfannkuchen <jasper-meiners@msn.com>
2015-05-16 13:17:41 +0900

    setup prototype yasu
    first code test of falling cubes

---
+++ Overflow/Assets/overflow/prototype_yasu/scripts/cubeGeneration.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class cubeGeneration : MonoBehaviour {
+	
+	public GameObject cubePrefab;
+	public float spawnRange = 2f;
+	public int maxPrefabs = 100;
+
+	GameObject[,] cubeArr = new GameObject[8,8];
+	
+	void Start(){
+	}
+	
+	void Update() {
+		if (Input.GetMouseButtonDown(0)){
+			int x = Random.Range(0, 7);
+			int y = Random.Range(0, 7);
+			Debug.Log ("cubeGenerator wants to create new cube at: " + x + "," + y);
+
+			Vector3 spawnPosition = new Vector3(x, 20f, y);
+			
+			GameObject cube = Instantiate(cubePrefab, spawnPosition, Quaternion.identity) as GameObject;
+			cube.transform.SetParent(this.transform);
+			
+			cubeArr[x,y] = cube;
+
+			Debug.Log ("cube created at: " + x + "," + y);
+		}
+	}
+}
