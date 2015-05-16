e6617f4c6f049a5d4d8aed852c680f0189cdf2c8
Pfannkuchen <jasper-meiners@msn.com>
2015-05-16 16:36:36 +0900

    reintegrate

--- Overflow/Assets/overflow/prototype_yasu/scripts/cubeController.cs
+++ Overflow/Assets/overflow/prototype_yasu/scripts/cubeController.cs
@@ -5,7 +5,9 @@ public class cubeController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		Rigidbody RB = GetComponent<Rigidbody> ();
+		Vector3 myVel = new Vector3 (0f, -10f, 0f);
+		RB.velocity = myVel;
 	}
 	
 	// Update is called once per frame
