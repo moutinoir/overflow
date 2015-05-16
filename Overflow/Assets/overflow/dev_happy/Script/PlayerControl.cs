607c99b5d44372d6358dd36404dbc6b2caf14725
Happy2042 <is0228he@ed.ritsumei.ac.jp>
2015-05-16 13:27:29 +0900

    prototype happy

---
+++ Overflow/Assets/overflow/dev_happy/Script/PlayerControl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using InControl;
+
+
+
+		public class PlayerControl : MonoBehaviour
+		{
+			void Update()
+			{
+				// Use last device which provided input.
+				var inputDevice = InputManager.ActiveDevice;
+				
+				// Rotate target object with left stick.
+				transform.Translate( Vector3.down,  500.0f * Time.deltaTime * inputDevice.LeftStickX, Space.World );
+				transform.Translate( Vector3.right, 500.0f * Time.deltaTime * inputDevice.LeftStickY, Space.World );
+			}
+		}
+
+
