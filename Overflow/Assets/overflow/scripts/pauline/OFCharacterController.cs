14726af20016edcaabe326e2b797b51b2989aa02
pauline <machabert.pauline@gmail.com>
2015-05-16 08:41:19 +0200

    add standards assets, put fps controller in my prototype scene

---
+++ Overflow/Assets/overflow/scripts/pauline/OFCharacterController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OFCharacterController : MonoBehaviour 
+{
+	public float MoveSpeed = 0;
+	CharacterController motionController;
+	// Use this for initialization
+	void Awake () 
+	{
+		motionController = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Move ();
+	}
+
+	void Move ()
+	{
+
+	}
+}
