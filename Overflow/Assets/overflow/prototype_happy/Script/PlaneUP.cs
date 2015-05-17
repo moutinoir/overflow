da0af935e4f66c748c53788b2e0a4884443cd6d5
Pfannkuchen <jasper-meiners@msn.com>
2015-05-17 16:52:43 +0900

    bug fixing and materials for walls

--- Overflow/Assets/overflow/prototype_happy/Script/PlaneUP.cs
+++ Overflow/Assets/overflow/prototype_happy/Script/PlaneUP.cs
@@ -3,23 +3,26 @@ using System.Collections;
 
 public class PlaneUP : MonoBehaviour {
 
+	public GameObject gameOverPrefab;
+
 	public float speed;
 
 	// Update is called once per frame
 	void Update () {
-
 		// Plane UP moving
 		transform.Translate (0.0f,  speed * 0.01f, 0.0f, Space.World);
-
 	}
 
 	void OnCollisionEnter (Collision collision){
 
 		// collision
-		Debug.Log ("collision OK!!");
+		Debug.Log ("PLAYER fell into the water!");
 		// only "Player" destroy
+
 		if(collision.gameObject.CompareTag("Player")){
 			Destroy(collision.gameObject);
+
+			Instantiate (gameOverPrefab, Vector3.zero, Quaternion.identity);
 		}
 	}
 }
